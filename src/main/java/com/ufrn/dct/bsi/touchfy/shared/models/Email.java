bfc00084cc479b5ceaae2a8261c83629032668d9
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-04-20 16:56:11 -0300

    [feat](#1): adiciona value object de email

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/shared/models/Email.java
@@ -0,0 +1,20 @@
+package com.ufrn.dct.bsi.touchfy.shared.models;
+
+import java.util.regex.Pattern;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Email {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+    private static final Pattern PATTERN = Pattern.compile(EMAIL_REGEX);
+    private final String value;
+
+    public Email(String value) {
+        if (value == null || !PATTERN.matcher(value).matches())
+            throw new IllegalArgumentException("Email inválido");
+        this.value = value;
+    }
+}
