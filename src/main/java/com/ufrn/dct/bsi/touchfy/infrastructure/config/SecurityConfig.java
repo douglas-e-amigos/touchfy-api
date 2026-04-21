9c77d55441c3aa4374d1525d81eceb0c001a613f
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-04-20 21:01:51 -0300

    (chore)[#1]: desabilita spring security temporariamente

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.ufrn.dct.bsi.touchfy.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
