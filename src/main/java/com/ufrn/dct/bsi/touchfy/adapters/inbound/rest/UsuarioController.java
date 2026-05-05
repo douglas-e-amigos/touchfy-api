d1d032c9761d042373334ebab8007086d9269a48
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-05-05 10:03:54 -0300

    (feat)[#3]: cria endpoint de login

--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/UsuarioController.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/UsuarioController.java
@@ -1,7 +1,9 @@
 package com.ufrn.dct.bsi.touchfy.adapters.inbound.rest;
 
 import com.ufrn.dct.bsi.touchfy.adapters.inbound.rest.routes.UsuarioRoute;
+import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.AutenticarUsuarioRequest;
 import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.CriarUsuarioRequest;
+import com.ufrn.dct.bsi.touchfy.application.usecases.usuario.AutenticarUsuarioUseCase;
 import com.ufrn.dct.bsi.touchfy.application.usecases.usuario.CriarUsuarioUseCase;
 import com.ufrn.dct.bsi.touchfy.shared.dtos.NovoRecursoResponse;
 
@@ -17,17 +19,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @RestController
 @RequestMapping(UsuarioRoute.ROOT)
 @AllArgsConstructor
 public class UsuarioController {
     private final CriarUsuarioUseCase criarUsuarioUseCase;
+    private final AutenticarUsuarioUseCase autenticarUsuarioUseCase;
 
-    @PostMapping
+    @PostMapping(UsuarioRoute.CADASTRO)
     public ResponseEntity<NovoRecursoResponse> criarUsuario(
             @RequestBody @Valid final CriarUsuarioRequest request
-            ) {
+    ) {
         criarUsuarioUseCase.execute(request);
 
         return ResponseEntity.ok(NovoRecursoResponse.builder()
@@ -36,5 +40,11 @@ public class UsuarioController {
                 .mensagem("Usuario criado com sucesso!")
                 .build()
         );
+    }
+
+    @PostMapping(UsuarioRoute.LOGIN)
+    public ResponseEntity<?> autenticarUsuario(@RequestBody @Valid final AutenticarUsuarioRequest request) {
+        final String token = autenticarUsuarioUseCase.execute(request.nomeUsuario(), request.senha());
+        return ResponseEntity.ok(Map.of("token", token));
     }
 }
--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/routes/UsuarioRoute.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/routes/UsuarioRoute.java
@@ -2,4 +2,6 @@ package com.ufrn.dct.bsi.touchfy.adapters.inbound.rest.routes;
 
 public class UsuarioRoute {
     public final static String ROOT = "/usuarios";
+    public final static String CADASTRO = "/auth/register";
+    public final static String LOGIN = "/auth/login";
 }
