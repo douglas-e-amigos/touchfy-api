4b4135e119fc261d22b2b7cfdfea0649d9d8c8d0
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-05-05 14:31:10 -0300

    (feat)[#3]: cria endpoint de atualizar usuário

--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/UsuarioController.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/UsuarioController.java
@@ -1,32 +1,35 @@
 package com.ufrn.dct.bsi.touchfy.adapters.inbound.rest;
 
 import com.ufrn.dct.bsi.touchfy.adapters.inbound.rest.routes.UsuarioRoute;
+import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.AtualizarUsuarioRequest;
 import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.AutenticarUsuarioRequest;
 import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.CriarUsuarioRequest;
+import com.ufrn.dct.bsi.touchfy.application.usecases.usuario.AtualizarUsuarioUseCase;
 import com.ufrn.dct.bsi.touchfy.application.usecases.usuario.AutenticarUsuarioUseCase;
 import com.ufrn.dct.bsi.touchfy.application.usecases.usuario.CriarUsuarioUseCase;
 import com.ufrn.dct.bsi.touchfy.shared.dtos.NovoRecursoResponse;
 
+import com.ufrn.dct.bsi.touchfy.shared.dtos.RecursoAtualizadoResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(UsuarioRoute.ROOT)
 @AllArgsConstructor
 public class UsuarioController {
-    private final CriarUsuarioUseCase criarUsuarioUseCase;
+    private final AtualizarUsuarioUseCase atualizarUsuarioUseCase;
     private final AutenticarUsuarioUseCase autenticarUsuarioUseCase;
+    private final CriarUsuarioUseCase criarUsuarioUseCase;
 
     @PostMapping(UsuarioRoute.CADASTRO)
     public ResponseEntity<NovoRecursoResponse> criarUsuario(
@@ -35,7 +38,7 @@ public class UsuarioController {
         criarUsuarioUseCase.execute(request);
 
         return ResponseEntity.ok(NovoRecursoResponse.builder()
-                .criado(true)
+                .criado(Boolean.TRUE)
                 .criadoEm(LocalDate.now())
                 .mensagem("Usuario criado com sucesso!")
                 .build()
@@ -46,5 +49,19 @@ public class UsuarioController {
     public ResponseEntity<?> autenticarUsuario(@RequestBody @Valid final AutenticarUsuarioRequest request) {
         final String token = autenticarUsuarioUseCase.execute(request.nomeUsuario(), request.senha());
         return ResponseEntity.ok(Map.of("token", token));
+    }
+
+    @SecurityRequirement(name = "bearerAuth")
+    @PatchMapping(UsuarioRoute.ATUALIZAR)
+    public ResponseEntity<RecursoAtualizadoResponse> atualizarUsuario(@PathVariable("id") final UUID id,
+            @RequestBody @Valid final AtualizarUsuarioRequest request) {
+        atualizarUsuarioUseCase.execute(id, request);
+
+        return ResponseEntity.ok(RecursoAtualizadoResponse.builder()
+                .atualizado(Boolean.TRUE)
+                .atualizadoEm(LocalDate.now())
+                .mensagem("Usuário atualizado com sucesso!")
+                .build()
+        );
     }
 }
