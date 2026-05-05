706efc0c85fdea4b9d036d9355c07362196f313d
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-05-05 14:28:03 -0300

    (feat)[#3]: adiciona mapeamento dto atualização para entity

--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/mappers/UsuarioMapper.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/mappers/UsuarioMapper.java
@@ -1,11 +1,11 @@
 package com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.mappers;
 
 import com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.entities.UsuarioEntity;
+import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.AtualizarUsuarioRequest;
 import com.ufrn.dct.bsi.touchfy.application.dtos.usuario.CriarUsuarioRequest;
 import com.ufrn.dct.bsi.touchfy.domain.usuario.models.Usuario;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = {ImagemMapper.class, EmailMapper.class})
 public interface UsuarioMapper {
@@ -15,7 +15,11 @@ public interface UsuarioMapper {
     @Mapping(source = "imagem", target = "caminhoDaImagemDePerfil", qualifiedByName = "mapImageToPath")
     UsuarioEntity toEntity(Usuario usuario);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(AtualizarUsuarioRequest atualizarUsuarioRequest, @MappingTarget UsuarioEntity entity);
+
     @Mapping(target = "imagem",  ignore = true)
     @Mapping(source = "email", target = "email", qualifiedByName = "mapValueToEmail")
     Usuario toDomain(CriarUsuarioRequest request);
+
 }
