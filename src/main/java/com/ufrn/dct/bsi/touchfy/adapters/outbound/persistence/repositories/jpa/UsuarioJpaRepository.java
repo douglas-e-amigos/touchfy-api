2c5a66153767ffc28cedf2b668a7e81ce5bf3df0
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-05-05 10:01:04 -0300

    (feat)[#3]: recupera usuário pelo username

--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/repositories/jpa/UsuarioJpaRepository.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/repositories/jpa/UsuarioJpaRepository.java
@@ -3,7 +3,9 @@ package com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.repositories.jpa;
 import com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.entities.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioJpaRepository extends JpaRepository<UsuarioEntity, UUID> {
+    Optional<UsuarioEntity> findByNomeUsuario(String nome);
 }
--- src/main/java/com/ufrn/dct/bsi/touchfy/domain/usuario/repository/UsuarioRepository.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/domain/usuario/repository/UsuarioRepository.java
@@ -3,6 +3,10 @@ package com.ufrn.dct.bsi.touchfy.domain.usuario.repository;
 import com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.entities.UsuarioEntity;
 import com.ufrn.dct.bsi.touchfy.domain.usuario.models.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository {
     UsuarioEntity salvar(Usuario usuario);
+
+    Optional<UsuarioEntity> acharPeloNomeDeUsuario(final String nomeUsuario);
 }
