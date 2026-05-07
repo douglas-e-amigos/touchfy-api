9db20ae5f7be7e20466d5ae8b5ce3c68f218a440
Mosiah Adam <tech.adam.servicos@gmail.com>
2026-05-07 14:27:42 -0300

    (feat)[#3]: cria enum para os diretórios de armazenamento de arquivos

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/application/enums/DiretorioStorage.java
@@ -0,0 +1,12 @@
+package com.ufrn.dct.bsi.touchfy.application.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum DiretorioStorage {
+    FOTOS_DE_PERFIL("usuarios/%s/perfil");
+
+    private final String diretorio;
+}
