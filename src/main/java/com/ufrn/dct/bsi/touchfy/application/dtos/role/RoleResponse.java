8a0880ca442b3f03792df93bc59c042e12a85387
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-07 19:12:02 -0300

    [feat](#25): adiciona DTOs para criação e atualização de papéis com permissões

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/application/dtos/role/RoleResponse.java
@@ -0,0 +1,11 @@
+package com.ufrn.dct.bsi.touchfy.application.dtos.role;
+
+import com.ufrn.dct.bsi.touchfy.application.dtos.permission.PermissionResponse;
+import com.ufrn.dct.bsi.touchfy.domain.role.ERole;
+import java.util.List;
+
+public record RoleResponse(
+        Long id,
+        ERole name,
+        List<PermissionResponse> permissions
+) {}
