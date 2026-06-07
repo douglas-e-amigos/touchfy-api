6e5733a4221c08a4e4b73d8a763453e71e73f32b
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-07 19:14:04 -0300

    [feat](#25): implementa controlador de papéis com operações CRUD

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/inbound/rest/RoleController.java
@@ -0,0 +1,77 @@
+package com.ufrn.dct.bsi.touchfy.adapters.inbound.rest;
+
+import com.ufrn.dct.bsi.touchfy.application.dtos.role.CreateRoleRequest;
+import com.ufrn.dct.bsi.touchfy.application.dtos.role.RoleResponse;
+import com.ufrn.dct.bsi.touchfy.application.dtos.role.UpdateRoleRequest;
+import com.ufrn.dct.bsi.touchfy.application.usecases.role.*;
+import com.ufrn.dct.bsi.touchfy.shared.dtos.NovoRecursoResponse;
+import com.ufrn.dct.bsi.touchfy.shared.dtos.RecursoAtualizadoResponse;
+import com.ufrn.dct.bsi.touchfy.shared.dtos.RecursoDeletadoResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/roles")
+@AllArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
+public class RoleController {
+
+    private final CriarRoleUseCase criarRoleUseCase;
+    private final BuscarRolePorIdUseCase buscarRolePorIdUseCase;
+    private final ListarRolesUseCase listarRolesUseCase;
+    private final AtualizarRoleUseCase atualizarRoleUseCase;
+    private final ExcluirRoleUseCase excluirRoleUseCase;
+
+    @PostMapping
+    public ResponseEntity<NovoRecursoResponse> criar(@RequestBody @Valid final CreateRoleRequest request) {
+        criarRoleUseCase.execute(request);
+        return ResponseEntity.ok(NovoRecursoResponse.builder()
+                .criado(Boolean.TRUE)
+                .criadoEm(LocalDate.now())
+                .mensagem("Perfil criado com sucesso!")
+                .build()
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleResponse>> listar() {
+        final List<RoleResponse> roles = listarRolesUseCase.execute();
+        return ResponseEntity.ok(roles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleResponse> buscarPorId(@PathVariable final Long id) {
+        final RoleResponse role = buscarRolePorIdUseCase.execute(id);
+        return ResponseEntity.ok(role);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RecursoAtualizadoResponse> atualizar(
+            @PathVariable final Long id,
+            @RequestBody @Valid final UpdateRoleRequest request
+    ) {
+        atualizarRoleUseCase.execute(id, request);
+        return ResponseEntity.ok(RecursoAtualizadoResponse.builder()
+                .atualizado(Boolean.TRUE)
+                .atualizadoEm(LocalDate.now())
+                .mensagem("Perfil atualizado com sucesso!")
+                .build()
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<RecursoDeletadoResponse> excluir(@PathVariable final Long id) {
+        excluirRoleUseCase.execute(id);
+        return ResponseEntity.ok(new RecursoDeletadoResponse(
+                "Perfil excluído com sucesso!",
+                Boolean.TRUE,
+                LocalDate.now()
+        ));
+    }
+}
