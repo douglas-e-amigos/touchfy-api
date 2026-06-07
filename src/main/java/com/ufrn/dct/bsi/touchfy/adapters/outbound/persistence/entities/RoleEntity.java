23801c85fd436e53ba1659f352f8e8effe6057cb
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-07 18:29:36 -0300

    [feat](#25): cria entidades Role e ERole para gerenciamento de papéis e permissões

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/entities/RoleEntity.java
@@ -0,0 +1,28 @@
+package com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.entities;
+
+import com.ufrn.dct.bsi.touchfy.domain.role.ERole;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private ERole name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "roles_permissions",
+        joinColumns        = @JoinColumn(name = "role_id"),
+        inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<PermissionEntity> permissions;
+}
