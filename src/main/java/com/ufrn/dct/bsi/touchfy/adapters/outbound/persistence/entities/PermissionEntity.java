7dcb74b19bd7314b964ea2ac264f65dbd3159d30
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-07 18:29:17 -0300

    [feat](#25): cria entidades Permission e PermissionEntity para gerenciamento de permissões

---
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/entities/PermissionEntity.java
@@ -0,0 +1,17 @@
+package com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "permissions")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class PermissionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
