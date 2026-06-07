773ca6671ba144a00d03039329ad8a820a13a65b
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-07 18:28:46 -0300

    [feat](#25): implementa tabelas de RBAC e remove tabelas legadas

---
+++ src/main/resources/db/migration/V7__remove_tabelas_legadas_rbac.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS papel_do_usuario;
+DROP TABLE IF EXISTS permissao_do_papel;
+DROP TABLE IF EXISTS permissao_do_usuario;
+DROP TABLE IF EXISTS permissao;
+DROP TABLE IF EXISTS papel;
