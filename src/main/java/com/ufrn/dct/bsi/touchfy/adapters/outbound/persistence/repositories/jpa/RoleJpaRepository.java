42cd6d199b04dc3e33acc6569de396d0a4da22ac
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-08 10:41:54 -0300

    [feat](#25): adiciona método para buscar papel por nome no repositório de papéis

--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/repositories/jpa/RoleJpaRepository.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/repositories/jpa/RoleJpaRepository.java
@@ -1,9 +1,14 @@
 package com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.repositories.jpa;
 
 import com.ufrn.dct.bsi.touchfy.adapters.outbound.persistence.entities.RoleEntity;
+import com.ufrn.dct.bsi.touchfy.domain.role.ERole;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleJpaRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity> findByName(ERole name);
 }
