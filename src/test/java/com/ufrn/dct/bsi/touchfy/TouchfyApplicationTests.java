b352210efb94f22e67e7558d81f512c77a740da6
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-06-08 19:19:12 -0300

    [feat](#25): ativa o Flyway para migrações de banco de dados nos testes

--- src/test/java/com/ufrn/dct/bsi/touchfy/TouchfyApplicationTests.java
+++ src/test/java/com/ufrn/dct/bsi/touchfy/TouchfyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-	"spring.flyway.enabled=false",
+	"spring.flyway.enabled=true",
 	"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
 	"spring.datasource.username=sa",
 	"spring.datasource.password="
