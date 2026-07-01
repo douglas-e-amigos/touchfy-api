7f9c02db8e732e27271119d363119b92a5405c71
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-07-01 19:32:19 -0300

    feat: adiciona campos de timestamp e traceId ao ErroResponse

--- src/main/java/com/ufrn/dct/bsi/touchfy/shared/dtos/ErroResponse.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/shared/dtos/ErroResponse.java
@@ -1,7 +1,11 @@
 package com.ufrn.dct.bsi.touchfy.shared.dtos;
 
+import java.time.Instant;
+
 public record ErroResponse(
         String mensagem,
-        int statusCode
+        int statusCode,
+        Instant timestamp,
+        String traceId
 ) {
 }
