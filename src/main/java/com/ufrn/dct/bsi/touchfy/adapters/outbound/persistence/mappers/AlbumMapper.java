87508010dbbff5426c47d5e61a75431e3002ca5e
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-07-02 13:06:06 -0300

    feat: adiciona mapeamento de tipo de álbum e método para criar álbum a partir de música avulsa

--- src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/mappers/AlbumMapper.java
+++ src/main/java/com/ufrn/dct/bsi/touchfy/adapters/outbound/persistence/mappers/AlbumMapper.java
@@ -8,11 +8,13 @@ import com.ufrn.dct.bsi.touchfy.application.dtos.album.AlbumResponse;
 import com.ufrn.dct.bsi.touchfy.application.dtos.album.AlbumSalvoResponse;
 import com.ufrn.dct.bsi.touchfy.domain.album.models.Album;
 import com.ufrn.dct.bsi.touchfy.domain.album.models.AlbumSalvo;
+import com.ufrn.dct.bsi.touchfy.domain.album.models.TipoAlbum;
 import com.ufrn.dct.bsi.touchfy.domain.musica.models.GeneroMusical;
 import com.ufrn.dct.bsi.touchfy.domain.musica.models.Musica;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface AlbumMapper {
@@ -22,6 +24,7 @@ public interface AlbumMapper {
 
   @Mapping(target = "musicas", source = "musicas")
   @Mapping(target = "generoMusical", source = "generoMusical")
+  @Mapping(target = "tipo", source = "tipo", qualifiedByName = "mapTipo")
   AlbumResponse toResponse(Album album);
 
   AlbumSalvo toSalvoDomain(AlbumSalvoEntity entity);
@@ -40,5 +43,23 @@ public interface AlbumMapper {
 
   default List<AlbumSalvo> toSalvoDomainList(List<AlbumSalvoEntity> entities) {
     return entities.stream().map(this::toSalvoDomain).toList();
+  }
+
+  @Named("mapTipo")
+  default TipoAlbum mapTipo(TipoAlbum tipo) {
+    return tipo == null ? TipoAlbum.ALBUM : tipo;
+  }
+
+  default Album toAlbumDeSingleAvulso(final Musica musica) {
+    return Album.builder()
+        .id(null)
+        .nome(musica.getNome())
+        .descricao(null)
+        .dataLancamento(null)
+        .generoMusical(null)
+        .artistaId(null)
+        .tipo(TipoAlbum.SINGLE)
+        .musicas(List.of(musica))
+        .build();
   }
 }
