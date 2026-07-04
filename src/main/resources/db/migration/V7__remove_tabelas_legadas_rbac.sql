880af68ffd586d3cbb19ba8db0db88cc2d0fdfae
Paulo Douglas <paulodouglasmdias@gmail.com>
2026-07-04 16:48:48 -0300

    refactor: remove migrações obsoletas do banco de dados

--- src/main/resources/db/migration/V7__remove_tabelas_legadas_rbac.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS papel_do_usuario;
-DROP TABLE IF EXISTS permissao_do_papel;
-DROP TABLE IF EXISTS permissao_do_usuario;
-DROP TABLE IF EXISTS permissao;
-DROP TABLE IF EXISTS papel;
